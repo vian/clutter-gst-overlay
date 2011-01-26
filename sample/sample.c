dc2e1d31fb08f5acd699ad2d79f82e255b781c4a
Vadim Zakondyrin <thekondr@crystalnix.com>
2011-01-26 13:03:01 +0600

    Updated sample

--- sample/sample.c
+++ sample/sample.c
@@ -8,6 +8,30 @@ gcc -o sample/sample sample/sample.c clutter-gst-overlay/clutter-gst-overlay-act
 #include <clutter/clutter.h>
 #include "../clutter-gst-overlay/clutter-gst-overlay-actor.h"
 
+/*
+static void
+restart_video (GstElement *playbin,
+               gpointer    user_data)
+{
+  ClutterGstOverlayActor *gst_actor = CLUTTER_GST_OVERLAY_ACTOR (user_data);
+  gchar *uri = get_uri (gst_actor);
+  gdouble duration = get_duration (gst_actor);
+  //  set_uri (gst_actor, uri);
+  g_print ("Restarting... %s %g\n", uri, GST_TIME_AS_SECONDS (duration));
+
+  g_free (uri);
+  gst_element_set_state (playbin, GST_STATE_PAUSED);
+  set_progress (gst_actor, 0.5);
+  gst_element_set_state (playbin, GST_STATE_PLAYING);
+}
+*/
+
+void my_close (ClutterMedia *media,
+            gpointer      user_data)
+{
+  clutter_media_set_progress (CLUTTER_MEDIA (user_data), 0.0);
+}
+
 int main (int argc, char *argv[])
 {
   ClutterColor stage_color = { 0x00, 0xFF, 0x00, 0xFF };
@@ -27,10 +51,9 @@ int main (int argc, char *argv[])
   ClutterActor *rect = clutter_gst_overlay_actor_new ();
   clutter_actor_set_size (rect, 440, 280);
   clutter_actor_set_position (rect, 100, 100);
-  g_object_set (G_OBJECT (rect), "uri", argv[1],
-		"subtitle-uri", argv[2],
-		"subtitle-font-name", "Monospace bold italic 24",
-		NULL);
+
+  clutter_media_set_uri (CLUTTER_MEDIA (rect), argv[1]);
+  clutter_media_set_subtitle_uri (CLUTTER_MEDIA (rect), argv[2]);
 
   //  ClutterActor *cont = clutter_group_new ();
   //  clutter_actor_set_position (cont, 200, 200);
@@ -40,8 +63,11 @@ int main (int argc, char *argv[])
 
   clutter_actor_show (stage);
 
-  //clutter_gst_overlay_actor_play (CLUTTER_GST_OVERLAY_ACTOR (rect));
-  g_object_set (G_OBJECT (rect), "playing", TRUE, "audio-volume", 1.0, NULL);
+  clutter_media_set_playing (CLUTTER_MEDIA (rect), TRUE);
+  clutter_media_set_subtitle_font_name (CLUTTER_MEDIA (rect), "Sans bold italic 32");
+
+  g_signal_connect (CLUTTER_MEDIA (rect), "eos",
+                    G_CALLBACK (my_close), rect);
 
   clutter_main ();
 
